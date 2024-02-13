5507c07b4f55e56d16b168e7a4a3deaaba5f46d5
WSL2 Ubuntu Furuya <inf.4clover@gmail.com>
2024-02-13 23:53:49 +0900

    test laravel phpunit

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -18,4 +18,10 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /** @test */
+    public function これはテストです()
+    {
+        $this->assertTrue(true);
+    }
 }
