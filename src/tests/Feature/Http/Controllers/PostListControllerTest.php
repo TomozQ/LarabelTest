59f0513f7b643c329cba0a6925720ce15d9f776e
WSL2 Ubuntu Furuya <inf.4clover@gmail.com>
2024-02-23 21:37:46 +0900

    topページ

--- src/tests/Feature/Http/Controllers/PostListControllerTest.php
+++ src/tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -2,16 +2,31 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostListControllerTest extends TestCase
 {
+   use RefreshDatabase;
    /** @test */
    function blog_list_display()
    {
-        $this->get('/')
-             ->assertOk();
+      // $post1 = Post::factory()->create();
+      // $post2 = Post::factory()->create();
+
+      // $this->get('/')
+      //       ->assertOk()
+      //       ->assertSee($post1->title)
+      //       ->assertSee($post2->title);
+
+      $post1 = Post::factory()->create(['title' => 'ブログのタイトル1']);
+      $post2 = Post::factory()->create(['title' => 'ブログのタイトル2']);
+
+      $this->get('/')
+            ->assertOk()
+            ->assertSee('ブログのタイトル1')
+            ->assertSee('ブログのタイトル2');
    }
 }
